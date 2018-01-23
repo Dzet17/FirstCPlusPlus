cb6720060585040689a6083c00fa43fcfbac7834
Dylan Zettler <dylanzettler@gmail.com>
2018-01-23 09:51:45 -0700

    Changed and learned about how to make the runner multiple ways

---
+++ FirstCPlusPlus/Controller/Runner.cpp
@@ -0,0 +1,21 @@
+//
+//  Runner.cpp
+//  FirstCPlusPlus
+//
+//  Created by Zettler, Dylan on 1/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include <iostream>
+#include "Controller.hpp"
+
+int main()
+{
+    Controller * app = new Controller();
+    
+    (*app).start();
+    
+    app->start();
+    
+    return 0;
+}
